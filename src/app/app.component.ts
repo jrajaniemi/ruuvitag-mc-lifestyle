a2d9a28d407bef2f6cab31da80817cb29ca33f66
Jussi Rajaniemi <jrajaniemi@mc-lifestyle.com>
2018-03-16 12:14:36 +0200

    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'Scrambler Weather Station';
+
+  constructor(private route: ActivatedRoute) {
+
+  }
+
+  ngOnInit() {
+    this.title = this.route.snapshot.queryParams[""];
+  }
 }
