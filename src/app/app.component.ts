30430a05d72cbf1e3672bce2bae9ad68892ee354
Jussi Rajaniemi <jrajaniemi@mc-lifestyle.com>
2018-03-17 09:26:57 +0200

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,44 @@ import {
   Params
 } from '@angular/router';
 
-// https://github.com/Equim-chan/base91
+function base91_decode(s: string): string {
+  let dec_table = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '#', '$',
+    '%', '&', '(', ')', '*', '+', ',', '.', '/', ':', ';', '<', '=',
+    '>', '?', '@', '[', ']', '^', '_', '`', '{', '|', '}', '~', '"'
+  ];
+  let debug = 1;
+  let l = s.length;
+  let v = -1;
+  let b;
+  let n;
+  let o = '';
+  for (let i=0; i < l; ++i) {
+                                                if (debug) console.log(i + '. merkki ' + s.charAt(i));
+    let c = s.charCodeAt(i);                    if (debug) console.log('1: '+c);
 
-import * as base91 from 'base91';
+    if (!c) continue;
+    if (v < 0) {
+      v = c;                                    if (debug) console.log('2: '+v);
+    } else {
+      v += c * 91;                              if (debug) console.log('2: '+v);
+      b |=  (v << n);                           if (debug) console.log('3: '+b);
+      n += (v & 8191) > 88 ? 13 : 14;           if (debug) console.log('4: '+n);
+      do {
+        o += String.fromCharCode(b & 255);      if (debug) console.log('5: '+o);
+        b = b >> 8;                             if (debug) console.log('6: '+b);
+        n -= 8;                                 if (debug) console.log('7: '+n);
+      } while (n > 7);
+      v = -1;                                   if (debug) console.log('8: '+c);
+    }
+  }
+  if (v + 1)
+    o += String.fromCharCode((b | v << n) & 255); if (debug) console.log('1: '+o);
+  return ""+o;
+}
 
 @Component({
   selector: 'app-root',
@@ -29,9 +64,10 @@ export class AppComponent implements OnInit {
 
   ngOnInit() {
     this.route.fragment.subscribe((hash: string) => {
-      if (hash.length === 9) {
+      console.log('Hash: '+hash+' Hash Length: '+hash.length);
+      if (hash.length === 8) {
         console.log('fragment = ' + hash + '; length = ' + hash.length);
-        let decoded = base91.decode(hash);
+        let decoded = base91_decode(hash);
         console.log('91: ' + decoded);
         this.temp = Number(atob(hash).substr(0, 2));
         this.humidity = Number(atob(hash).substr(2, 2));
